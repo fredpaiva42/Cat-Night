33d0c3b95b619cae4301a3fb2bc0b9f9951e7327
fredpaiva42 <fredericogdpaiva@gmail.com>
2022-01-21 00:09:53 -0300

    Ajustando a largura para 1300

--- code/settings.py
+++ code/settings.py
@@ -5,7 +5,7 @@ tile_size = 32
 GAME_STATE = 0
 
 screen_height = vertical_tile_qtd * tile_size
-screen_width = 1450
+screen_width = 1300
 
 flags = pygame.SCALED
 screen = pygame.display.set_mode((screen_width, screen_height), flags)
